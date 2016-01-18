d8637d897f989d25807825bd4762ed7825d32dcb
delight26 <ljb0426@naver.com>
2016-01-18 20:35:05 +0900

    프로젝트 삭제

--- BookManagement/src/com/bookmanage/controller/CommandAction.java
+++
@@ -1,9 +0,0 @@
-package com.bookmanage.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface CommandAction {
-	public ViewInfo requestProc(HttpServletRequest request, HttpServletResponse response, ViewInfo view)
-			throws Throwable;
-}
