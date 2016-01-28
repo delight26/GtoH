ad9bf241744b68e25795499caaca185c303ed8bc
delight26 <ljb0426@naver.com>
2016-01-28 18:28:35 +0900

    add 쿼리 완료

--- ProjectCall/src/main/java/com/project/call/junbum/daoimpl/JBDaoImpl.java
+++ ProjectCall/src/main/java/com/project/call/junbum/daoimpl/JBDaoImpl.java
@@ -48,6 +48,6 @@ public class JBDaoImpl implements JBDao {
 	@Override
 	public void addProduct(PointProduct p) {
 		SqlParameterSource prodparam = new BeanPropertySqlParameterSource(p);
-		namedParameterJdbcTemplate.update("insert into product values(0, :pName, :pPrice, , :pAmount)", prodparam);
+		namedParameterJdbcTemplate.update("insert into product values(0, :pName, :pPrice, '', :pAmount, :pImage, 0)", prodparam);
 	}
 }
