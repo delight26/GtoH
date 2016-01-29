37cdfa76d63fd59d00c7cc78f23f0a65183b49a2
khskhsa <khskhss@naver.com>
2016-01-29 14:28:05 +0900

    회원약관

--- ProjectCall/src/main/java/com/project/call/hyunsu/controller/HSController.java
+++ ProjectCall/src/main/java/com/project/call/hyunsu/controller/HSController.java
@@ -2,16 +2,28 @@ package com.project.call.hyunsu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project.call.hyunsu.service.HSService;
 
 @Controller
+@RequestMapping(value="/hyunsu")
 public class HSController {
 	
 	@Autowired
-	private HSService jBService;
+	private HSService service;
 	
-	public void setjBService(HSService jBService) {
-		this.jBService = jBService;
+	public void setService(HSService service) {
+		this.service = service;
+	}
+	
+	@RequestMapping(value="/addMember/step1")
+	public String addMemberStep1(){
+		return "member/test2";
+	}
+	
+	@RequestMapping(value="/addMember/step2")
+	public String addMemberSetp2(){
+		return "home";
 	}
 }
--- ProjectCall/src/main/webapp/WEB-INF/views/member/login.jsp
+++ ProjectCall/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -5,6 +5,7 @@
 <head>
 <meta charset="UTF-8">
 <title>술배틀 Login</title>
+<script src="resources/js/addMember.js"></script>
 </head>
 <body>
 	<form action="loginresult" method="post">
@@ -17,6 +18,7 @@
 				<th>비밀번호:</th>
 				<td><input type="password" name="pass" /></td>
 				<td><input type="submit" value="로그인" /></td>
+				<td><input type="button" value="회원가입" onclick="addmember()" /></td>
 			</tr>
 		</table>
 	</form>
