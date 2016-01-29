481abad5c47b84365d54b020c13f7c117ddc98af
jws <jws@WanSOOn>
2016-01-29 12:25:18 +0900

    목록, 조회

--- ProjectCall/src/main/java/com/project/call/wansoon/dao/WSDao.java
+++ ProjectCall/src/main/java/com/project/call/wansoon/dao/WSDao.java
@@ -1,5 +1,14 @@
 package com.project.call.wansoon.dao;
 
+import java.util.List;
+
+import com.project.call.domain.FreeBoard;
+
 public interface WSDao {
 
+	public List<FreeBoard> getFreeBoardAll();
+	
+	public FreeBoard getFreeBoard(int frbNo);
+	
+	public int FreeBoardCount();
 }
